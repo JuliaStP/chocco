f6bbc5f492c007792d665ab28a27c4ad6e6b15e3
Julia Andreeva <andreeva_julia@hotmail.com>
2020-07-05 13:18:02 -0400

    fixed margins, perfect pixel

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// BURGER MENU /////////////////////////////////////////////
+
 const burgerBtn = document.querySelector('#burger');
 const hamburger = document.querySelector('.hamburger');
 const closeBtn = document.querySelector('.hamburger__btn');
@@ -23,7 +25,7 @@ closeBtn.addEventListener('click', e => {
 });
 
 
-//team//////////////////////////////////////////////////
+// TEAM //////////////////////////////////////////////////
 
 const team = document.querySelector('.team');
 const teamLink = document.querySelector('.team__link');
@@ -52,30 +54,7 @@ team.addEventListener('click', e => {
 }
 });
 
-
 //SLIDER/////////////////////////////////////////////////////////////////
-// const leftBtn = document.querySelector('.arrow--left');
-// const rightBtn = document.querySelector('.arrow--right');
-// const slider = document.querySelector('.goods-list');
-// const computedStyles = getComputedStyle(slider);
-
-// leftBtn.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   let currentRight = parseInt(computedStyles.right);
-
-//   if (currentRight > 0) {
-//       slider.style.right = `${currentRight - 940}px`;
-//   }
-// });
-
-// rightBtn.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   let currentRight = parseInt(computedStyles.right)
-
-//   if(currentRight < 940) {
-//       slider.style.right = `${currentRight + 940}px`;
-//   }
-// });
 
 const slider = $('.goods-list').bxSlider({
   pager: false,
@@ -93,7 +72,7 @@ $('.arrow__link--right').click(e => {
 })
 
 
-// reviews ///////////////////////////////////////////////
+// REVIEWS ///////////////////////////////////////////////
 
 const findBlockByAlias = (alias) => {
   return $('.review__container').filter((ndx, item) => {
@@ -113,79 +92,7 @@ $('.avatar-menu__link').click((e) => {
   curItem.addClass('active').siblings().removeClass('active');
 });
 
-// form  /////////////////////////////////////////////////////////
-
-// const myForm = document.querySelector('#form');
-// const sendBtn = document.querySelector('#send');
-// const xhr = new XMLHttpRequest();
-
-
-// xhr.addEventListener('load', () => {
-//   if (xhr.response.status) {
-//       console.log('correct!');
-//   }
-// });
-
-// sendBtn.addEventListener('click', function(e) {
-//   e.preventDefault();
-
-//   if (validateForm(myForm)) {
-
-//     const data = {
-//       name: myForm.elements.name.value,
-//       phone: myForm.elements.phone.value,
-//       comment: myForm.elements.email.value
-//   }
-
-//   const xhr = new XMLHttpRequest();
-//     xhr.responseType = 'json';
-//     xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-//     xhr.send(JSON.stringify(data));
-//     xhr.addEventListener('load', () => {
-//       if (xhr.response.status) {
-//         console.log('correct!');
-//     }
-// });
-// }
-// });
-
-
-// function validateForm(form) {
-//   let valid = true; 
-
-//   if (!validateField(form.elements.name)) {
-//     valid = false;
-// }
-
-// if (!validateField(form.elements.phone)) {
-//   valid = false;
-// }
-
-// if (!validateField(form.elements.street)) {
-//   valid = false;
-// }
-
-// if (!validateField(form.elements.house)) {
-//   valid = false;
-// }
-
-// if (!validateField(form.elements.apt)) {
-//   valid = false;
-// }
-
-// if (!validateField(form.elements.comment)) {
-//   valid = false;
-// }
-
-// if (!validateField(form.elements.radio)) {
-//   valid = false;
-// }
-
-// return valid;
-// }
-
-
-// popup///////////////////////////////////////////
+// FORM ///////////////////////////////////////////
 
 const validateFields = (form, fieldsArray) => {
 
